c1467bc528ebea91a382faab29aa7997668e0089
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-07-30 13:31:21 +0530

    Add triangle validation and electricity bill calculation programs

--- Conditions and Decisions/ConditionsAndDecisions13.java
+++ Conditions and Decisions/ConditionsAndDecisions13.java
@@ -1,9 +1,7 @@
-
 import java.util.Scanner;
 
-public class ConditionsAndDecisions11 {
-
-        public static void main(String[] args) {
+public class ConditionsAndDecisions13 {
+     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
     
             System.out.println("Enter a firsttriangle :");
@@ -13,18 +11,15 @@ public class ConditionsAndDecisions11 {
             System.out.println("Enter a thirdtriangle :");
             int thirdTriangle = sc.nextInt();
     
-            if(firstTriangle==secondTriangle && firstTriangle==thirdTriangle
-               && 
-               secondTriangle==thirdTriangle 
-            ){
-                System.out.println("Euilateral Triangle");
+            if(firstTriangle != secondTriangle || secondTriangle != thirdTriangle
+                || firstTriangle != thirdTriangle ){
+                System.out.println("Scalene Triangle");
             }else{
     
-                System.out.println("Ineuilateral Triangle");
+                System.out.println("Not Scalene Triangle");
                 
             }
     
             sc.close();
         }
-    
 }
