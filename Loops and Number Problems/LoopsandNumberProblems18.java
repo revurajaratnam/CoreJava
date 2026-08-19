0f85ca2a3f988b19222def701fd933148ffda3d6
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-19 21:41:35 +0530

    added

--- Loops and Number Problems/LoopsandNumberProblems18.java
+++ Loops and Number Problems/LoopsandNumberProblems18.java
@@ -2,6 +2,7 @@ public class LoopsandNumberProblems18 {
     public static void main(String[] args) {
         System.out.println("Added new statement");
         int i =0;
+        System.out.println("");
        while(i <10){
         System.out.println(i);
         i++;
