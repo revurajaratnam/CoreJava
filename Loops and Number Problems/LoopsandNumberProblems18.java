b35cdc39088b78a84e7db10e35a1ed46481b5193
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-19 21:30:46 +0530

    Remove unused loop and print statements from main method of LoopsandNumberProblems18 class

--- Loops and Number Problems/LoopsandNumberProblems18.java
+++ Loops and Number Problems/LoopsandNumberProblems18.java
@@ -2,12 +2,7 @@ public class LoopsandNumberProblems18 {
     public static void main(String[] args) {
         System.out.println("Added new statement");
         int i =0;
-        while(i<5){
-            System.out.println(i);
-            i++;
-        }
-        System.out.println("            ");
-        System.out.println("            ");
+       
 
     }
 }
