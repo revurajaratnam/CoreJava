e4dcf7d2694efcc616265311a6406d6de191a41a
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-25 15:39:14 +0530

    Update output statement in main method of LoopsandNumberProblems19 class

--- Loops and Number Problems/LoopsandNumberProblems19.java
+++ Loops and Number Problems/LoopsandNumberProblems19.java
@@ -1,7 +1,7 @@
 public class LoopsandNumberProblems19 {
     public static void main(String[] args) {
        System.out.println(" ");
-       System.out.println();
+       System.out.println("");
 
 
 
