4e19e21c538db4342d15b432b7f9461fe459ea9e
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-08-27 15:17:09 +0530

    Remove redundant output statements in main method of LoopsandNumberProblems19 class

--- Loops and Number Problems/LoopsandNumberProblems19.java
+++ Loops and Number Problems/LoopsandNumberProblems19.java
@@ -2,8 +2,7 @@ public class LoopsandNumberProblems19 {
     public static void main(String[] args) {
        System.out.println(" ");
        System.out.println("");
-       System.out.println("");
-       System.out.println("");
+       
 
 
 
