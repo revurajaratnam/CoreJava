7708b4c37e5b8fc75b488fbf51590634981a0fce
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-09-02 22:18:26 +0530

    Remove for loop implementation from main method of LoopsandNumberProblems20 class

--- Loops and Number Problems/LoopsandNumberProblems20.java
+++ Loops and Number Problems/LoopsandNumberProblems20.java
@@ -1,11 +1,7 @@
 public class LoopsandNumberProblems20 {
     public static void main(String[] args) {
     
-        for(int i =1 ; i <= 15 ;i++){
-            System.out.println(i);
-        }
-        System.out.println("loop is completed.");
-       
+        
 
     }
 }
