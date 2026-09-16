1218ae8446ee0c3104c13bb0c68ead305b4cf9e5
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-09-16 22:23:38 +0530

    Update loop initialization in main method of LoopsandNumberProblems20 class to start from 1

--- Loops and Number Problems/LoopsandNumberProblems20.java
+++ Loops and Number Problems/LoopsandNumberProblems20.java
@@ -1,9 +1,10 @@
 public class LoopsandNumberProblems20 {
     public static void main(String[] args) {
     
-        int  i =0;
+        int  i =1;
         while(i<=10){
             System.out.println(i);
+            i++;
         }
 
 
