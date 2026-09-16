ae8e35b9888aa25626f96c42388adeb49ab655c6
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-09-16 22:21:52 +0530

    Add while loop to print numbers from 0 to 10 in main method of LoopsandNumberProblems20 class

--- Loops and Number Problems/LoopsandNumberProblems20.java
+++ Loops and Number Problems/LoopsandNumberProblems20.java
@@ -1,6 +1,10 @@
 public class LoopsandNumberProblems20 {
     public static void main(String[] args) {
     
+        int  i =0;
+        while(i<=10){
+            System.out.println(i);
+        }
 
 
 
