b260e62e8650d0ebecf0b8d260cc3a3a63c0bf62
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-09-22 19:35:29 +0530

    Remove empty print statements in main method of LoopsandNumberProblems21 class

--- Loops and Number Problems/LoopsandNumberProblems21.java
+++ Loops and Number Problems/LoopsandNumberProblems21.java
@@ -2,8 +2,7 @@ public class LoopsandNumberProblems21 {
     public static void main(String[] args) {
     
       
-        System.out.println("");
-        System.err.println();
+       
         
 
 
